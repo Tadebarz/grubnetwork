e2adf406b67eab9a7004df935e9f5d06b8a6f778
JowiSK <jnmartenss@gmail.com>
2024-05-07 16:35:31 +0200

    (feat/hero): adding features, animations

--- src/components/UI/organisms/Hero.jsx
+++ src/components/UI/organisms/Hero.jsx
@@ -1,9 +1,11 @@
 import React, { useRef, useEffect, useState } from 'react';
 import discord from '../../../assets/discord.svg'
+import { motion, useScroll } from 'framer-motion';
 
 const Hero = () => {
   const [isVisible, setIsVisible] = useState(false);
   const heroRef = useRef(null);
+  const { scrollY } = useScroll();
 
   useEffect(() => {
     const options = {
@@ -26,24 +28,40 @@ const Hero = () => {
     return () => observer.unobserve(heroRef.current);
   }, []);
 
+  const heroVariants = {
+    visible: { opacity: 100, x: 0 },
+    hiddenL: { opacity: 0, x: -100 },
+    hiddenR: { opacity: 0, x: 100 },
+  };
+
   return (
-    <div ref={heroRef} className={`relative h-screen transition-opacity ${isVisible ? 'opacity-100' : 'opacity-0'}`}>
-      <div className='absolute inset-0 flex items-center justify-center opacity-40 bg-[#c5bbb1] skew-y-6 h-[700px] mt-[-100px]'></div>
+    <div ref={heroRef} className={`relative h-screen transition-opacity ${isVisible ? 'opacity-100' : 'opacity-100'}`}>
+      <div className={`absolute inset-0 flex items-center justify-center opacity-30 bg-[#c5bbb1] skew-y-6 h-[700px] mt-[-100px] ${isVisible ? 'opacity-100' : 'opacity-0'}`}></div>
       <div className='text-center text-4xl absolute inset-0 flex flex-col items-center justify-center h-full'>
-        <div className='relative w-full h-full'>
-          <div className="absolute inset-0 h-full bg-[#242424] opacity-50 backdrop-blur-md"></div>
-          <div className="px-[200px] relative z-10 w-[60%] uppercase flex flex-col justify-center h-full">
-            <div className='flex flex-col items-start justify-start'>
-              <p className='text-9xl text-start font-bold'>Welcome to Grub Network</p>
-              {/* <p className='mb-[20px] text-7xl text-start font-bold'>Your Premier Gaming Server Hosting Provider</p> */}
-            </div>
-            <span className='mt-[25px] p-2 bg-gradient-to-r from-transparent via-[#b8b0a9] from-5% to-95% to-transparent text-4xl font-extrabold '>Elevating your gaming experience.</span>
-            <div className='flex justify-around w-[50%]'>
-              <b className='w-auto px-[20px] bg-[#d49860] text-white py-2 mt-[50px] border-[2px] border-[#e4dad1]'>SHOP</b>
-              <b className='flex items-center w-auto h-[60px] px-[20px] bg-[#5865F2] text-white py-2 mt-[50px] border-[2px] border-[#e4dad1]'>
-                <img src={discord} className='object-fill h-full'/>
-                <p>DISCORD</p>
-                </b>
+        <div className='relative w-full h-full flex'>
+
+          <div className="px-[200px] relative z-10 w-[60%] uppercase flex flex-col justify-center h-full" >
+            <motion.div className='flex flex-col items-start justify-start text-9xl text-start font-bold' variants={heroVariants} initial="hidden" animate={isVisible ? "visible" : "hiddenL"} transition={{ease: 'easeInOut', duration: 0.2}}>
+              <p className=''>Welcome to</p>
+              <p className='bg-gradient-to-r from-[rgb(242,46,49)] via-[#f08c3b] to-[#ff1e8b] text-transparent bg-clip-text'>Grub Network</p>
+            </motion.div>
+            <motion.div className='mt-[20px]' variants={heroVariants} initial="hidden" animate={isVisible ? "visible" : "hiddenR"}>
+              
+              <span className='mt-[25px] p-2 bg-gradient-to-r from-transparent via-[#b8b0a9] from-5% to-95% to-transparent text-4xl font-extrabold '>Elevating your gaming experience.</span>
+              <div className='flex justify-start w-full'>
+                <b className='rounded-md w-auto px-[20px] bg-gradient-to-r from-[rgb(242,46,49)] to-[#f08c3b] text-white py-2 mt-[50px] border-[1px] border-[#e4dad1] mr-[40px]'>SHOP</b>
+                <b className='flex items-center w-auto h-[60px] pl-[20px] pr-[20px] bg-[#5865F2] text-white py-2 mt-[50px] border-[1px] border-[#e4dad1] rounded-md'>
+                  <img src={discord} className='h-full w-auto mr-[10px]'/>
+                  <p>DISCORD</p>
+                  </b>
+              </div>
+            </motion.div>
+            
+          </div>
+
+          <div className='w-[40%] h-full px-[200px] relative z-10 uppercase flex flex-col justify-center'>
+            <div className='bg-black h-[100px] w-[100px] rounded-[40px]'>
+
             </div>
           </div>
         </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,14 +3,17 @@ import Navbar from '../components/UI/molecules/Navbar';
 import Hero from '../components/UI/organisms/Hero';
 import Shop from '../components/UI/organisms/Shop';
 import OurServers from '../components/UI/organisms/OurServers';
-import russiaedit from '../assets/hero-2.mp4';
+import russiaedit from '../assets/editiin.mp4';
 
 const Home = () => {
   return (
-    <div className='h-screen overflow-y-auto snap-y-mandatory text-white uppercase' style={{ scrollBehavior: 'smooth' }}>
+    <div className='h-screen  overflow-y-auto snap-y-mandatory text-white uppercase' style={{ scrollBehavior: 'smooth' }}>
+      
       <video autoPlay muted loop className='absolute inset-0 w-full h-full object-cover blur'>
         <source src={russiaedit} type="video/mp4" />
       </video>
+      <div className="absolute inset-0 h-full bg-[#242424] opacity-50 backdrop-blur-md"></div>
+
       <Navbar />
       <div className='snap-start h-full' style={{ scrollSnapAlign: 'start' }}>
         <Hero />
